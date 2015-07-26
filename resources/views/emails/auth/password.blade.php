e7497066b54f6a96145293ab919bdc88f65a9bc7
Taylor Otwell <taylorotwell@gmail.com>
2015-07-26 15:12:01 -0500

    working on emials

--- resources/views/emails/auth/password.blade.php
+++ resources/views/emails/auth/password.blade.php
@@ -1,1 +1,17 @@
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+Hi {{ explode(' ', $user->name)[0] }}!
+
+<br><br>
+
+Here is a link allowing you to reset your password: {{ url('password/reset/'.$token) }}
+
+<br><br>
+
+If you did not request a link to reset your password, please let us know.
+
+<br><br>
+
+Thanks!
+
+<br>
+
+{{ Laravel\Spark\Spark::generateInvoicesWith()['product'] }}
--- resources/views/emails/billing/invoice.blade.php
+++ resources/views/emails/billing/invoice.blade.php
@@ -1,1 +1,15 @@
-Your invoice is attached!
+Hi {{ explode(' ', $user->name)[0] }}!
+
+<br><br>
+
+Thanks for your continued support. We've attached a copy of this month's invoice for your review.
+Please let us know if you have any questions or concerns!
+
+<br><br>
+
+Thanks!
+
+<br>
+
+{{ $invoiceData['product'] }}
+
