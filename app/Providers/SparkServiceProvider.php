1ad82ad42caac38f535e6d4d5e87c8188c6c3eb9
Taylor Otwell <taylorotwell@gmail.com>
2015-07-26 14:32:07 -0500

    work on exports

--- app/Providers/SparkServiceProvider.php
+++ app/Providers/SparkServiceProvider.php
@@ -51,18 +51,18 @@ class SparkServiceProvider extends ServiceProvider
         $this->loadViewsFrom(SPARK_PATH.'/resources/views', 'spark');
 
         $this->publishes([
-            SPARK_PATH.'/resources/views' => base_path('resources/views/vendor/spark'),
-        ]);
-
-        $this->publishes([
             SPARK_PATH.'/resources/assets' => base_path('resources/assets/vendor/spark'),
         ], 'spark-assets');
 
         $this->publishes([
+            SPARK_PATH.'/resources/views' => base_path('resources/views/vendor/spark'),
+        ], 'spark-full');
+
+        $this->publishes([
             SPARK_PATH.'/resources/views/app.blade.php' => base_path('resources/views/vendor/spark/app.blade.php'),
             SPARK_PATH.'/resources/views/settings/tabs/profile.blade.php' => base_path('resources/views/vendor/spark/settings/tabs/profile.blade.php'),
-            SPARK_PATH.'/resources/views/auth/registration/subscription/basics.blade.php' => base_path('resources/views/vendor/spark/auth/registration/subscription/basics.blade.php'),
             SPARK_PATH.'/resources/views/auth/registration/simple/basics.blade.php' => base_path('resources/views/vendor/spark/auth/registration/simple/basics.blade.php'),
+            SPARK_PATH.'/resources/views/auth/registration/subscription/basics.blade.php' => base_path('resources/views/vendor/spark/auth/registration/subscription/basics.blade.php'),
         ], 'spark-basics');
     }
 
