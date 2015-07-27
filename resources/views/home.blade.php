d6e2ea9578af611001b937a0b9b2373b21cd1cf2
Taylor Otwell <taylorotwell@gmail.com>
2015-07-27 15:34:27 -0500

    work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
-					Your Application Dashboard.
+					Your Application's Dashboard.
 				</div>
 			</div>
 		</div>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -13,7 +13,7 @@
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
 				@if (Auth::check())
-					<li><a href="/">Home</a></li>
+					<li><a href="/home">Home</a></li>
 				@else
 					<!-- Guest -->
 				@endif
