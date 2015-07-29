5d806ae60805385a2dfa15ec752e8cdeee73f507
Taylor Otwell <taylorotwell@gmail.com>
2015-07-29 12:05:56 -0500

    fixes

--- resources/views/settings/tabs/subscription/modals/change/selector.blade.php
+++ resources/views/settings/tabs/subscription/modals/change/selector.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<!-- Monthly / Available Plans -->
 	<div class="col-md-6">
-		<div style="margin-bottom: 15px;">
+		<div style="margin-bottom: 15px;" class="spark-plan-change-selector-heading">
 			<span v-if="includesBothPlanIntervals"><strong>Monthly Plans</strong></span>
 			<span v-if=" ! includesBothPlanIntervals"><strong>Available Plans</strong></span>
 		</div>
@@ -13,7 +13,7 @@
 
 	<!-- Yearly Plans -->
 	<div class="col-md-6" v-if="includesBothPlanIntervals">
-		<div style="margin-bottom: 15px;">
+		<div style="margin-bottom: 15px;" class="spark-plan-change-selector-heading">
 			<strong>Yearly Plans</strong>
 		</div>
 
