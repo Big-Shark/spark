6ca5c90acf82f830801d0f0379c90c9303c7e816
Taylor Otwell <taylorotwell@gmail.com>
2015-07-30 16:02:24 -0500

    working on footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,6 +34,25 @@
 	<!-- Main Content -->
 	@yield('content')
 
+	<div style="margin-top: 125px;">
+		<!-- Push Footer -->
+	</div>
+
+	<footer class="footer">
+		<div class="container" style="color: #777;">
+			<div class="pull-left" style="padding-top: 28px;">
+				Copyright © Your Company
+			</div>
+
+			<div class="pull-right" style="padding-top: 18px; font-size: 32px;">
+				<i class="fa fa-btn fa-facebook-square"></i>
+                <i class="fa fa-twitter-square"></i>
+			</div>
+
+			<div class="clearfix"></div>
+		</div>
+	</footer>
+
 	<!-- Footer Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -7,15 +7,17 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/">Laravel</a>
+			<a class="navbar-brand" href="/" style="padding-top: 19px;">
+				<i class="fa fa-btn fa-sun-o"></i>Spark
+			</a>
 		</div>
 
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
 				@if (Auth::guest())
+					<li><a href="/home">Home</a></li>
 					<!-- Guest -->
 				@else
-					<li><a href="/home">Home</a></li>
 				@endif
 			</ul>
 
