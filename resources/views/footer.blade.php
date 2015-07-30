e787764a6f76183723423162a6a78e976b58f11b
Taylor Otwell <taylorotwell@gmail.com>
2015-07-30 16:29:25 -0500

    first

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+<div style="margin-top: 125px;">
+	<!-- Push Footer -->
+</div>
+
+<footer class="footer">
+	<div class="container" style="color: #777;">
+		<div class="pull-left" style="padding-top: 28px;">
+			Copyright © Your Company
+		</div>
+
+		<div class="pull-right" style="padding-top: 18px; font-size: 32px;">
+			<i class="fa fa-btn fa-facebook-square"></i>
+            <i class="fa fa-btn fa-twitter-square"></i>
+            <i class="fa fa-github-square"></i>
+		</div>
+
+		<div class="clearfix"></div>
+	</div>
+</footer>
