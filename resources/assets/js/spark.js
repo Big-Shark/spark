61bde9f4e94eca6f846097e945cd76707b091181
Taylor Otwell <taylorotwell@gmail.com>
2015-08-07 16:04:32 -0500

    saving progress

---
+++ resources/assets/js/spark.js
@@ -0,0 +1,66 @@
+module.exports = {
+	el: '#spark-app',
+
+    /*
+     * Bootstrap the application. Load the initial data.
+     */
+	ready: function () {
+		this.getUser();
+		this.getTeams();
+	},
+
+
+    /*
+     * Define the components.
+     */
+	components: {
+		'spark-errors': require('./common/errors'),
+		'spark-nav-bar': require('./nav'),
+		'spark-simple-register-screen': require('./auth/registration/simple'),
+		'spark-subscription-register-screen': require('./auth/registration/subscription'),
+		'spark-settings-screen': require('./settings/dashboard'),
+		'spark-team-settings-screen': require('./settings/team')
+	},
+
+
+	events: {
+		updateUser: function () {
+			console.log('Received Request To Update User.');
+
+			this.getUser();
+		},
+
+		teamsUpdated: function (teams) {
+			console.log('Teams Updated: Broadcasting...');
+
+			this.$broadcast('teamsRetrieved', teams);
+		}
+	},
+
+
+	methods: {
+		/**
+		 * Retrieve the user from the API and broadcast it to children.
+		 */
+		getUser: function () {
+			this.$http.get('/spark/api/users/me')
+				.success(function(user) {
+					console.log('Spark User Retrieved: Broadcasting...');
+
+					this.$broadcast('userRetrieved', user);
+				});
+		},
+
+        /*
+         * Get all of the user's current teams from the API.
+         */
+        getTeams: function () {
+		    this.$http.get('/spark/api/teams')
+		        .success(function (teams) {
+		        	console.log('Spark Teams Retrieved: Broadcasting...');
+
+					this.$broadcast('teamsRetrieved', teams);
+		        });
+		}
+	}
+};
