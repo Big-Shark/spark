1969717d85bd24e31ae8e1f0f18a94cd841281e6
Taylor Otwell <taylorotwell@gmail.com>
2015-08-11 13:07:45 -0400

    saving progress

--- app/Console/Install.php
+++ app/Console/Install.php
@@ -49,11 +49,15 @@ class Install extends Command
             ]
         );
 
-        if ($this->option('force') || $this->confirm('Would you like to install NPM dependencies now?')) {
+        if ($this->option('force') || $this->confirm('Would you like to run your database migrations?', 'yes')) {
+            (new Process('php artisan migrate', base_path()))->setTimeout(null)->run();
+        }
+
+        if ($this->option('force') || $this->confirm('Would you like to install your NPM dependencies?', 'yes')) {
             (new Process('npm install', base_path()))->setTimeout(null)->run();
         }
 
-        if ($this->option('force') || $this->confirm('Would you like to run Gulp now?')) {
+        if ($this->option('force') || $this->confirm('Would you like to run Gulp?', 'yes')) {
             (new Process('gulp', base_path()))->setTimeout(null)->run();
         }
 
