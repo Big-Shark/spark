575e37d0250d4aa71c84a844463c5ad85918c272
Taylor Otwell <taylorotwell@gmail.com>
2015-08-31 11:54:15 -0500

    saving progress

--- resources/assets/js/core/components.js
+++ resources/assets/js/core/components.js
@@ -3,7 +3,6 @@ require('./../nav');
 require('./../auth/registration/simple');
 require('./../auth/registration/subscription');
 require('./../settings/dashboard');
-// require('./../settings/dashboard/profile');
 require('./../settings/dashboard/subscription');
 require('./../settings/dashboard/teams');
 require('./../settings/team');
