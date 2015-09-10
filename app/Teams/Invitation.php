5eed0fae2ffbe61eef7c5335a3edc7004668839d
Taylor Otwell <taylorotwell@gmail.com>
2015-09-10 13:38:39 -0500

    saving progress

--- app/Teams/Invitation.php
+++ app/Teams/Invitation.php
@@ -23,6 +23,13 @@ class Invitation extends Model
     protected $guarded = [];
 
     /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['token'];
+
+    /**
      * Get the team that owns the invitation.
      */
     public function team()
